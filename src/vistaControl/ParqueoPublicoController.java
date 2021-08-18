d56a1099c19a068ac0b5d04bf067a51c097f42fc
Gianni <70019070+G20-00@users.noreply.github.com>
2021-08-17 19:42:40 -0500

    change

--- src/vistaControl/ParqueoPublicoController.java
+++ src/vistaControl/ParqueoPublicoController.java
@@ -64,6 +64,6 @@ public class ParqueoPublicoController {
     	horaFinalTextField.setText(horaFinal);
     	double valorAPagar = parqueoPublico.calcularValorAPagar();
     	// To modify line
-    	valorAPagarTextField.setText(valorAPagar+"");
+    	valorAPagarTextField.setText(CURRENCY.format(valorAPagar));
     }    
 }
